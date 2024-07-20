6ff2d8636ee1c516f4d3a290c835dcba550415b9
BHAVESH GUPTA <35276600+bhavesh-g@users.noreply.github.com>
2024-07-20 18:14:19 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,32 @@
-const cacheName = 'one-more-light-cache-v1';
-const filesToCache = [
+const CACHE_NAME = 'one-more-light-cache-v1';
+const urlsToCache = [
     '/',
     '/index.html',
-    '/styles.css',
-    '/app.js',
     '/manifest.json',
-    '/logo.png'
+    '/path/to/your/icon-192x192.png',
+    '/path/to/your/icon-512x512.png',
+    'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
+    'https://unpkg.com/react/umd/react.production.min.js',
+    'https://unpkg.com/react-dom/umd/react-dom.production.min.js',
+    'https://unpkg.com/@babel/standalone/babel.min.js',
+    'https://code.jquery.com/jquery-3.6.0.min.js',
+    'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'
 ];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            return cache.addAll(filesToCache);
-        })
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
-});
-
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [cacheName];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
     );
 });
